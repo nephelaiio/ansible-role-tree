39d338e3dba4b1be33202dedf736e34b382eb028
Ted Cook <teodoro.cook@gmail.com>
2016-09-22 17:11:05 -0600

    Set full executable path in spec tests

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe command('tree --version'), if:  os[:family] != 'arch' do
+describe command('/usr/bin/tree --version'), if:  os[:family] != 'arch' do
   its(:exit_status) { should eq 0 }
 end
